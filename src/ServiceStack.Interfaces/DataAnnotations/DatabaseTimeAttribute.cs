17789c040386212fe6a8dec0f1c46e2ffac0f367
Bruce Cowan <bruce.cowan@aquaticinformatics.com>
2014-07-15 11:46:36 -0700

    Add DatabaseTimeAttribute which will be used to get database server timestamps.

---
+++ src/ServiceStack.Interfaces/DataAnnotations/DatabaseTimeAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ServiceStack.DataAnnotations
+{
+    /// <summary>
+    /// Used to indicate that a DateTimeOffset or DateTime property contains a
+    /// database server timestamp which is automatically updated on insert or update.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class DatabaseTimeAttribute : AttributeBase
+    {
+    }
+}
